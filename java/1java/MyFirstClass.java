857ae0466f718072c7e27fdb01f5f466bf45b41a
Abhishek Verma <ak8888780@gmail.com>
2023-06-24 22:31:07 +0530

    Java 1st program

---
+++ java/1java/MyFirstClass.java
@@ -0,0 +1,26 @@
+//Creating a First Java Class
+public class MyFirstClass { 
+//Step 1 
+/*
+ The name of this Java class is MyFirstClass.
+ You need to save this text in a file named MyFirstClass.java.
+ A Java class must be saved in a file that has the same name as your class with the extension .java. 
+ This is mandatory and is in fact very convenient because you do not need to open a file to know what class is written in it.
+ You can give this class any name as long as it does not start with a number
+ There is a convention though: the name of a Java class starts with a capital letter.
+ This is not mandatory but all Java developers follow this convention. When you become a seasoned Java developer, 
+ seeing a class that does not follow this convention will look weird to you.
+*/
+
+//step 2
+/*
+Preparing the Compilation of your First Class
+Compiling is the second step you need to follow after the creation of your first class.
+It consists of transforming the Java code you wrote in your MyFirstClass.java file into another format that can be executed. 
+The result of this transformation will be stored in another file created by the compiler.
+The name of this file will be MyFirstClass.class.
+*/
+    public static void main (String...args) {
+        System.out.println("Hey Abhii!!, Soon you'll master the Java langauge");
+}
+}
