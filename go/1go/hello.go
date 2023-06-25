87c3162d0d7f2b441a804a71f9f7db8b714c386a
Abhishek Verma <ak8888780@gmail.com>
2023-06-25 15:25:28 +0530

    write first program in Go

---
+++ go/1go/hello.go
@@ -0,0 +1,19 @@
+// add a new file using terminal
+// go mod init filename , this is important to run go program
+package main
+
+//Declare a main package (a package is a way to group functions,
+// and it's made up of all the files in the same directory).
+
+import "fmt"
+
+// Import the popular fmt package, which contains functions for formatting text, including printing to the console.
+// This package is one of the standard library packages you got when you installed Go.
+
+func main() {
+	// Implement a main function to print a message to the console.
+	// A main function executes by default when you run the main package.
+	fmt.Println("Hey abhii !! SOon you master the Golang !!!")
+}
+
+// to run the code type in terminal - go run .
